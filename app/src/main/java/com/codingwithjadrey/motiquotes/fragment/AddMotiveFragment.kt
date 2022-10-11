ac4ec24bf651c8eeef8da8150b5b7a1cd6df3358
Isaac Jadrey <jadreyisaac@gmail.com>
2022-10-11 16:37:18 +0300

    implemented the destination from the quotes list fragment to add qoute fragment

--- app/src/main/java/com/codingwithjadrey/motiquotes/fragment/AddMotiveFragment.kt
+++ app/src/main/java/com/codingwithjadrey/motiquotes/fragment/AddMotiveFragment.kt
@@ -1,11 +1,16 @@
 package com.codingwithjadrey.motiquotes.fragment
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.codingwithjadrey.motiquotes.databinding.FragmentAddMotiveBinding
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 class AddMotiveFragment : Fragment() {
 
@@ -19,6 +24,16 @@ class AddMotiveFragment : Fragment() {
     ): View? {
         _binding = FragmentAddMotiveBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val currentDate = LocalDateTime.now()
+        val format = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
+        val formattedDate = currentDate.format(format)
+        binding.currentDateTv.text = formattedDate
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/codingwithjadrey/motiquotes/fragment/MotiveListFragment.kt
+++ app/src/main/java/com/codingwithjadrey/motiquotes/fragment/MotiveListFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.codingwithjadrey.motiquotes.databinding.FragmentMotiveListBinding
+
 
 class MotiveListFragment : Fragment() {
 
@@ -19,6 +21,15 @@ class MotiveListFragment : Fragment() {
     ): View? {
         _binding = FragmentMotiveListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.addMotiveBtn.setOnClickListener {
+            val addMotiveQuote =
+                MotiveListFragmentDirections.actionMotiveListFragmentToAddMotiveFragment("Add Moti Quote")
+            findNavController().navigate(addMotiveQuote)
+        }
     }
 
     override fun onDestroyView() {
