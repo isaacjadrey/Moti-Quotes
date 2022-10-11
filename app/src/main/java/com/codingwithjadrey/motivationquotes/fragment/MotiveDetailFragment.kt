4364a363cfa748587085fbc66a1ea51831eeaa06
Isaac Jadrey <jadreyisaac@gmail.com>
2022-10-11 13:47:28 +0300

    Created fragments to list quotes, add quotes and view quotes. Added font used in the app

---
+++ app/src/main/java/com/codingwithjadrey/motivationquotes/fragment/MotiveDetailFragment.kt
@@ -0,0 +1,6 @@
+package com.codingwithjadrey.motivationquotes.fragment
+
+import androidx.fragment.app.Fragment
+
+class MotiveDetailFragment: Fragment() {
+}
---
+++ app/src/main/java/com/codingwithjadrey/motivationquotes/fragment/MotiveListFragment.kt
@@ -0,0 +1,7 @@
+package com.codingwithjadrey.motivationquotes.fragment
+
+import androidx.fragment.app.Fragment
+
+class MotiveListFragment : Fragment() {
+
+}
