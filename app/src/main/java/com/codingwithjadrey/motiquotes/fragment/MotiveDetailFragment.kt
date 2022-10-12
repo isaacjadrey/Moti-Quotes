35ac68acb8e552ecd8dc2738a43bcd2d7a8af41f
Isaac Jadrey <jadreyisaac@gmail.com>
2022-10-12 20:16:19 +0300

    handled click functions on the delete and edit buttons in detail fragment respectively

--- app/src/main/java/com/codingwithjadrey/motiquotes/fragment/MotiveDetailFragment.kt
+++ app/src/main/java/com/codingwithjadrey/motiquotes/fragment/MotiveDetailFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.codingwithjadrey.motiquotes.MotiQuoteApplication
 import com.codingwithjadrey.motiquotes.R
@@ -12,6 +13,7 @@ import com.codingwithjadrey.motiquotes.data.Motive
 import com.codingwithjadrey.motiquotes.databinding.FragmentMotiveDetailBinding
 import com.codingwithjadrey.motiquotes.model.QuotesViewModel
 import com.codingwithjadrey.motiquotes.model.QuotesViewModelFactory
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MotiveDetailFragment : Fragment() {
 
@@ -46,8 +48,33 @@ class MotiveDetailFragment : Fragment() {
             binding.apply {
                 detailMotiveDate.text = motive.createdOn
                 detailMotiveItem.text = motive.motiveQuote
+
+                detailDeleteBtn.setOnClickListener { showConfirmationDialog() }
+                detailEditBtn.setOnClickListener { editQuote() }
             }
         }
+    }
+
+    private fun showConfirmationDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(R.string.delete)
+            .setMessage(R.string.delete_question)
+            .setCancelable(false)
+            .setNegativeButton(R.string.no) {_,_ -> }
+            .setPositiveButton(R.string.yes) {_,_ -> deleteQuote() }
+            .show()
+    }
+
+    private fun deleteQuote() {
+        quotesViewModel.deleteQuote(motive)
+        findNavController().navigate(R.id.action_motiveDetailFragment_to_motiveListFragment)
+    }
+
+    private fun editQuote() {
+        val editDirections = MotiveDetailFragmentDirections.actionMotiveDetailFragmentToAddMotiveFragment(
+            "Edit Quote", motive.id
+        )
+        findNavController().navigate(editDirections)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
