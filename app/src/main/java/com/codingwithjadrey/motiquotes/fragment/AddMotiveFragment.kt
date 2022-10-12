6f513f7deeb82e1df11230da9eca4dbe89e514ce
Isaac Jadrey <jadreyisaac@gmail.com>
2022-10-12 20:36:23 +0300

    updated add motive fragment to retrive a quote item with its ID from the detail fragment and update the quote onSave

--- app/src/main/java/com/codingwithjadrey/motiquotes/fragment/AddMotiveFragment.kt
+++ app/src/main/java/com/codingwithjadrey/motiquotes/fragment/AddMotiveFragment.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.codingwithjadrey.motiquotes.MotiQuoteApplication
+import com.codingwithjadrey.motiquotes.R
+import com.codingwithjadrey.motiquotes.data.Motive
 import com.codingwithjadrey.motiquotes.databinding.FragmentAddMotiveBinding
 import com.codingwithjadrey.motiquotes.model.QuotesViewModel
 import com.codingwithjadrey.motiquotes.model.QuotesViewModelFactory
@@ -25,6 +29,8 @@ class AddMotiveFragment : Fragment() {
 
     private var _binding: FragmentAddMotiveBinding? = null
     private val binding get() = _binding!!
+    lateinit var motive: Motive
+    private val navigationArgs: AddMotiveFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,13 +44,24 @@ class AddMotiveFragment : Fragment() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val quoteId = navigationArgs.itemId
+        if (quoteId > 0) {
+            quotesViewModel.getQuote(quoteId).observe(this.viewLifecycleOwner) { quoteItem ->
+                motive = quoteItem
+                binding.apply {
+                    currentDateTv.setText(motive.createdOn, TextView.BufferType.SPANNABLE)
+                    motivationQuote.setText(motive.motiveQuote, TextView.BufferType.SPANNABLE)
+                    saveQuoteBtn.setOnClickListener { updateQuote() }
+                }
+            }
+        } else {
+            val currentDate = LocalDateTime.now()
+            val format = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
+            val formattedDate = currentDate.format(format)
+            binding.currentDateTv.text = formattedDate
 
-        val currentDate = LocalDateTime.now()
-        val format = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
-        val formattedDate = currentDate.format(format)
-        binding.currentDateTv.text = formattedDate
-
-        binding.saveQuoteBtn.setOnClickListener { validateEntry() }
+            binding.saveQuoteBtn.setOnClickListener { validateEntry() }
+        }
     }
 
     private fun validateEntry() {
@@ -61,6 +78,15 @@ class AddMotiveFragment : Fragment() {
         val quoteListFragment =
             AddMotiveFragmentDirections.actionAddMotiveFragmentToMotiveListFragment()
         findNavController().navigate(quoteListFragment)
+    }
+
+    private fun updateQuote() {
+        quotesViewModel.updateQuoteItem(
+            this.navigationArgs.itemId,
+            this.binding.currentDateTv.text.toString(),
+            this.binding.motivationQuote.text.toString()
+        )
+        findNavController().navigate(R.id.action_addMotiveFragment_to_motiveListFragment)
     }
 
     override fun onDestroyView() {
